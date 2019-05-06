2ec123ecdd2e77915ab859b7139997628fc37993
John Afolayan <jnafolayan@gmail.com>
2019-05-06 03:05:38 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+      { test: /\.vue$/, use: 'vue-loader' },
+      { test: /\.css$/, use: ['vue-style-loader', 'css-loader'] },
+    ]
+  },
+  devServer: {
+    open: true,
+    hot: true
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new VueLoaderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
